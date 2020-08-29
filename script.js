b136132dfdb91418977e02f980319a3771607013
Nauman Munir <naumanmunir@gmail.com>
2020-08-29 22:32:52 +0500

    final commity

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// function to get time in hours, mintues, and seconds
 function showTime() {
   let date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
 
-  let formatHours = convertFormat(hours);
+  let formatHours = AMPM(hours);
 
-  hours = checkTime(hours);
+  hours = convertTime(hours);
 
   hours = addZero(hours);
   minutes = addZero(minutes);
@@ -17,7 +18,8 @@ function showTime() {
   ).innerHTML = `${hours} : ${minutes} : ${seconds} ${formatHours}`;
 }
 
-function convertFormat(time) {
+// Adding functionality to by placing AM and PM according to the current time
+function AMPM(time) {
   let format = "AM";
 
   if (time >= 12) {
@@ -27,7 +29,8 @@ function convertFormat(time) {
   return format;
 }
 
-function checkTime(time) {
+// Adding functionality to the current time to convert 24 hours to 12 hours clock
+function convertTime(time) {
   if (time > 12) {
     time = time - 12;
   }
@@ -39,6 +42,7 @@ function checkTime(time) {
   return time;
 }
 
+// Adding functionality to show place "0" before numbers which are less than 10
 function addZero(time) {
   if (time < 10) {
     time = "0" + time;
